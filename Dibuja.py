09c3e79eba293b4232dd6a45645d53cae5e879fd
AleJNC <jessicalejandraninocardona@gmail.com>
2024-09-12 11:47:22 -0600

    Codigo para muestreo, aun no almacena imagenes

---
+++ Dibuja.py
@@ -0,0 +1,74 @@
+import os
+import cv2 as cv
+import numpy as np
+import pandas as pd
+from mediapipe.python.solutions.holistic import FACEMESH_CONTOURS, POSE_CONNECTIONS, HAND_CONNECTIONS
+from mediapipe.python.solutions.drawing_utils import draw_landmarks, DrawingSpec
+
+def draw_keypoints(image, results):
+    draw_landmarks(
+        image,
+        results.face_landmarks,
+        FACEMESH_CONTOURS,
+        DrawingSpec(color=(80, 110, 10), thickness=1, circle_radius=1),
+        DrawingSpec(color=(80, 256, 121), thickness=1, circle_radius=1),
+    )
+    # Draw pose connections
+    draw_landmarks(
+        image,
+        results.pose_landmarks,
+        POSE_CONNECTIONS,
+        DrawingSpec(color=(80, 22, 10), thickness=2, circle_radius=4),
+        DrawingSpec(color=(80, 44, 121), thickness=2, circle_radius=2),
+    )
+    # Draw left hand connections
+    draw_landmarks(
+        image,
+        results.left_hand_landmarks,
+        HAND_CONNECTIONS,
+        DrawingSpec(color=(121, 22, 76), thickness=2, circle_radius=4),
+        DrawingSpec(color=(121, 44, 250), thickness=2, circle_radius=2),
+    )
+    # Draw right hand connections
+    draw_landmarks(
+        image,
+        results.right_hand_landmarks,
+        HAND_CONNECTIONS,
+        DrawingSpec(color=(245, 117, 66), thickness=2, circle_radius=4),
+        DrawingSpec(color=(245, 66, 230), thickness=2, circle_radius=2),
+    )
+
+def guardaFrames(frames, output_folder):
+    for num_frame, frame in enumerate(frames):
+        frame_path = os.path.join(output_folder, f"{num_frame + 1}.jpg")
+        cv.imwrite(frame_path, cv.cvtColor(frame, cv.COLOR_BGR2BGRA))
+        
+def extract_keypoints(results):
+    pose = np.array([[res.x, res.y, res.z, res.visibility] for res in results.pose_landmarks.landmark]).flatten() if results.pose_landmarks else np.zeros(33*4)
+    face = np.array([[res.x, res.y, res.z] for res in results.face_landmarks.landmark]).flatten() if results.face_landmarks else np.zeros(468*3)
+    lh = np.array([[res.x, res.y, res.z] for res in results.left_hand_landmarks.landmark]).flatten() if results.left_hand_landmarks else np.zeros(21*3)
+    rh = np.array([[res.x, res.y, res.z] for res in results.right_hand_landmarks.landmark]).flatten() if results.right_hand_landmarks else np.zeros(21*3)
+    return np.concatenate([pose, face, lh, rh])
+
+def get_keypoints(model, sample_path):
+    kp_seq = np.array([])
+    for img_name in os.listdir(sample_path):
+        img_path = os.path.join(sample_path, img_name)
+        frame = cv.imread(img_path)
+        results = mediapipe_detection(frame, model)
+        kp_frame = extract_keypoints(results)
+        kp_seq = np.concatenate([kp_seq, [kp_frame]] if kp_seq.size > 0 else [[kp_frame]])
+    return kp_seq
+
+def insert_keypoints_sequence(df, n_sample:int, kp_seq):
+    for frame, keypoints in enumerate(kp_seq):
+        data = {'sample': n_sample, 'frame': frame + 1, 'keypoints': [keypoints]}
+        df_keypoints = pd.DataFrame(data)
+        df = pd.concat([df, df_keypoints])
+    return df
+
+def mediapipe_detection(image, model):
+    image = cv.cvtColor(image, cv.COLOR_BGR2RGB)
+    image.flags.writeable = False
+    results = model.process(image)
+    return results
---
+++ Muestras.py
@@ -0,0 +1,83 @@
+import cv2 as cv
+import numpy as np
+import os 
+
+from mediapipe.python.solutions.holistic import Holistic
+from datetime import datetime
+from Dibuja import draw_keypoints, mediapipe_detection, guardaFrames
+from typing import NamedTuple
+
+FONT = cv.FONT_HERSHEY_PLAIN
+FONT_SIZE = 1.5
+FONT_POS = (5, 30)
+
+MIN_LENGTH_FRAMES = 5
+LENGTH_KEYPOINTS = 1662
+MODEL_FRAMES = 15
+
+ROOT_PATH = os.getcwd()
+FRAME_ACTIONS_PATH = os.path.join(ROOT_PATH, "frame_actions")
+DATA_PATH = os.path.join(ROOT_PATH, "data")
+
+
+def creaCarpeta(path):
+    if not os.path.exists(path):
+        os.makedirs(path)
+        
+def hayMano(results: NamedTuple) -> bool:
+    return results.left_hand_landmarks or results.right_hand_landmarks
+
+
+def muestras(path, margenFrame =2, minFrames=7, delayFrames=3):
+    creaCarpeta(path)
+    frames=[]
+    framesComp=0
+    cuentaFrame=0
+    recording=False
+    
+    with Holistic() as holistic_model:
+        camera=cv.VideoCapture(0)
+        
+        while camera.isOpened():
+            ret, frame = camera.read()
+            if not ret: 
+                break
+            
+            img =frame.copy()
+            results= mediapipe_detection(frame, holistic_model)
+            
+            if hayMano(results) or recording:
+                recording=False
+                cuentaFrame += 1
+                if cuentaFrame > margenFrame:
+                     cv.putText(img, 'Almacenando', FONT_POS, FONT, FONT_SIZE, (255, 255, 255), thickness=1)
+                     frames.append(np.asarray(frame))
+                     
+                else:
+                    if len(frames) > minFrames + margenFrame:
+                        framesComp +=1 
+                        if framesComp < delayFrames:
+                            recording =True
+                            continue
+                        frames= frames[: - (margenFrame+ minFrames)]
+                        today = datetime.now().strftime('%y%m%d%H%M%S%f')
+                        carpetaSalida = os.path.join(path, f"Muestra_{today}")
+                        creaCarpeta(carpetaSalida)
+                        guardaFrames(frames, carpetaSalida)
+                        
+                recording, framesComp = False, 0
+                frames, cuentaFrame=[], 0
+                cv.putText(img, 'Listo para capturar de nuevo...', FONT_POS, FONT, FONT_SIZE, (0, 177, 90))
+                    
+            draw_keypoints(img, results)
+            cv.imshow(f'Toma de muestras para "{os.path.basename(path)}"', img)
+            if cv.waitKey(10)&0xFF ==ord('q'):
+                break
+            
+        camera.release()
+        cv.destroyAllWindows()
+        
+if __name__ == "__main__":
+    word_name = "buenos_dias"
+    word_path = os.path.join(ROOT_PATH, FRAME_ACTIONS_PATH, word_name)
+    muestras(word_path)

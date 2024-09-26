ddf36d0b1be76aeca364249432f1809a59a3a9ba
UsiGM <167590311+UsiGM@users.noreply.github.com>
2024-09-26 17:26:40 -0600

    Cambios en el codigo de LSTM.py

--- LSTM.py
+++ LSTM.py
@@ -1,9 +1,9 @@
 #Establecer librerias para el entrenamiendo de la red neuronal
 import numpy as np #Numpy se utiliza para reconocer los arreglos y poder manipularlos para su entrenamiento
-import h5py #H5py nos sirve para abrir los archivos tipo .h5 y de esta forma poder obtener sus datos
+import pandas as pd #Pandas se utiliza para tomar la información del archivo .h5 y lograr trabajarlo
 import tensorflow #Nos ayuda a realizar el proceso del entrenamiento del modelo con sus diferentes librerias
 
-from keras.models import Sequential  #Modelos, capas y utilidades 
+from keras.models import Sequential  #Modelos, capas y utilidades por parte de keras  
 from keras.layers import LSTM, Dense
 from keras.utils import to_categorical
 from sklearn.model_selection import train_test_split
@@ -11,14 +11,15 @@ from sklearn.model_selection import train_test_split
 path= "./data/keypoints/Adios.h5"
 
 def load_data(path):
-    with h5py.File(path, 'r') as f:
-        X= np.array(f['X'])
-        y= np.array(f['y'])
+    df = pd.read_hdf(path)
 
-        return X, y
+    X = df.iloc[:, :-1].values
+    y = df.iloc[:, -1].values
+
+    return X, y
     
-def preprocess_data(X, y, test_size= 0.2, random_state= 42):
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size, random_state)
+def preprocess_data(X, y, test_size= 0.2):
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=42)
 
     y_train = to_categorical(y_train)
     y_test = to_categorical(y_test)
@@ -41,16 +42,24 @@ def train_model(model, X_train, y_train, X_test, y_test, epochs=50, batch_size=3
 
 X, y = load_data(path)
 
-n_samples, timesteps, n_features = X.shape
+print(f"Dimensiones de X: {X.shape}")
+print(f"Dimensiones de y: {y.shape}")
+
+
+n_samples, n_features= X.shape
+timesteps = 1
 n_classes = len(np.unique(y))
 
 X_train, X_test, y_train, y_test = preprocess_data(X, y)
+
+X_train = X_train.reshape((X_train.shape[0], timesteps, n_features))
+X_test = X_test.reshape((X_test.shape[0], timesteps, n_features))
 
 model = create_lstm_model(timesteps, n_features, n_classes)
 
 train = train_model(model, X_train, y_train, X_test, y_test, epochs=50, batch_size=32)
 
-model.save('Modelo_Prueba_LSTM.h5')
+model.save('./Modelo_Prueba_LSTM.h5')
 
 loss, accuracy = model.evaluate(X_test, y_test)
 print(f"Perdida: {loss}, Precisión: {accuracy}")

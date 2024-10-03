750c3b4fc86949e6e5541b3eae32e27eb911a74d
AleJNC <jessicalejandraninocardona@gmail.com>
2024-10-03 15:37:22 -0600

    Constantes mod para frame_actions_5

--- Constantes.py
+++ Constantes.py
@@ -8,6 +8,6 @@ MODEL_FRAMES = 15
 
 # PATHS
 ROOT_PATH = os.getcwd()
-FRAME_ACTIONS_PATH = os.path.join(ROOT_PATH, "frame_actions")
+FRAME_ACTIONS_PATH = os.path.join(ROOT_PATH, "frame_actions_5")
 DATA_PATH = os.path.join(ROOT_PATH, "data_5M")
 KEYPOINTS_PATH = os.path.join(DATA_PATH, "keypoints")

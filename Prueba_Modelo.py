15769ae49ed0ca2b7931cc72f057374fd056cdef
AleJNC <jessicalejandraninocardona@gmail.com>
2024-10-18 12:02:30 -0600

    Palabra mostrada

--- Prueba_Modelo.py
+++ Prueba_Modelo.py
@@ -9,10 +9,8 @@ from Muestras import *
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 from gtts import gTTS
 import os
-import pygame
-from time import sleep
     
-def evaluar_model(src=None, threshold=0.8, margin_frame=1, delay_frames=3):
+def evaluar_modelo(src=None, threshold=0.8, margin_frame=1, delay_frames=1):
     kp_seq, sentence = [], []
     contenido=os.listdir('./data_5M/keypoints')
     contenido = np.array(contenido, dtype=str)
@@ -28,12 +26,12 @@ def evaluar_model(src=None, threshold=0.8, margin_frame=1, delay_frames=3):
         while video.isOpened():
             ret, frame = video.read()
             if not ret: break
-            results = DeteccionMediapipe(frame, holistic_model)
-            if hayMano(results) or recording:
+            resultados = DeteccionMediapipe(frame, holistic_model)
+            if hayMano(resultados) or recording:
                 recording = False
                 count_frame += 1
                 if count_frame > margin_frame:
-                    kp_frame = extraeKeypoints(results)
+                    kp_frame = extraeKeypoints(resultados)
                     kp_seq.append(kp_frame)
             else:
                 if count_frame >= LongMinFrames + margin_frame:
@@ -44,16 +42,18 @@ def evaluar_model(src=None, threshold=0.8, margin_frame=1, delay_frames=3):
                     kp_seq = kp_seq[: - (margin_frame + delay_frames)]
                     kp_normalized = normalizar_keypoints(kp_seq, int(FramesF))
                     res = model.predict(np.expand_dims(kp_normalized, axis=0))[0]
-                    print(np.argmax(res), f"({res[np.argmax(res)] * 100:.2f}%)")
+                    #print(np.argmax(res), f"({res[np.argmax(res)] * 100:.2f}%)")
+                    print(res[np.argmax(res)])
                     if res[np.argmax(res)] > threshold:
                         sent = word_ids[np.argmax(res)]
+                        print(sent)
                         sentence.insert(0, sent)
                         text_to_speech(sent)
                 recording, fix_frames, count_frame, kp_seq = False, 0, 0, []
             if not src:
-                cv2.rectangle(frame, (0, 0), (640, 35), (245, 117, 16), -1)
+                cv2.rectangle(frame, (0, 0), (640, 35), (100, 30, 140), -1)
                 cv2.putText(frame, ' | '.join(sentence), PosicionFuente, Fuente, TamFuente, (255, 255, 255))
-                DibujaKeypoints(frame, results)
+                DibujaKeypoints(frame, resultados)
                 cv2.imshow('Traductor LSM', frame)
                 if cv2.waitKey(10) & 0xFF == ord('q'):
                     break
@@ -61,4 +61,4 @@ def evaluar_model(src=None, threshold=0.8, margin_frame=1, delay_frames=3):
         cv2.destroyAllWindows()
         return sentence
 if __name__ == "__main__":
-    evaluar_model()
+    evaluar_modelo()
